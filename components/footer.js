f8d4b7f0e199f2db6df583c65c1a124a9b3e4c4b
blaze vincent <43787598+blaze-vincent@users.noreply.github.com>
2022-01-19 13:20:58 -0600

    Initial commit

---
+++ components/footer.js
@@ -0,0 +1,5 @@
+export default function Footer(){
+  return (<div>
+    footer tag contents
+  </div>)
+}
---
+++ components/header.js
@@ -0,0 +1,7 @@
+export default function Header(){
+  return (<div>
+    <h1 className="text-blue-700 text-2xl font-bold">
+      header tag content
+    </h1>
+  </div>)
+}
