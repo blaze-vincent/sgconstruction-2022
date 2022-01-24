932867a76c5a8267b4d7b9569cd340def49d24e1
Blaze Vincent <Blaze.Vincent@DRAKEHS.LOCAL>
2022-01-24 16:26:41 -0600

    Add footer content

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,71 @@
 export default function Footer(){
-  return (<div>
-    footer tag contents
+  return (<div
+    className="bg-neutral-900 py-6 px-3 text-white"
+  >
+    <div
+      className="flex flex-col items-center gap-4
+      sm:flex-row sm:justify-between 
+      max-w-screen-2xl m-auto"
+    >
+      <div
+        className="flex flex-col gap-2 items-center"
+      >
+        <div
+          className="flex flex-row gap-2 w-max"
+        >
+          <img
+            alt='Facebook icon'
+            src='/facebook_icon.svg'
+            className="w-7 h-7"
+          >
+          </img>
+          <img
+            alt='LinkedIn icon'
+            src='/linkedin_icon.svg'
+            className="w-7 h-7"
+          >
+          </img>
+        </div>
+        <img
+          alt='Butler General Contractor badge'
+          src='/butler_general_contractor.svg'
+          title='Butler General Contractor'
+          className="h-20"
+        ></img>
+        <p>© SG Construction Co.</p>
+      </div>
+      <div
+        className="flex flex-col w-max gap-4 sm:items-start sm:text-left text-center items-center"
+      >
+        <div>
+          <ul>
+            <li>
+              2204 W Mt Pleasant St.
+              <br></br>
+              West Burlington, IA 52655
+            </li>
+            <li>
+              Phone: <a href='tel:3197527154'>319.752.7154</a>
+            </li>
+            <li>
+              Email: <a href='mailto:info@sgconstructco.com'>info@sgconstructco.com</a>
+            </li>
+          </ul>
+        </div>
+        <div
+          className="flex flex-row gap-1"
+        >
+          <input 
+            type='text' 
+            placeholder="Search"
+            className="px-3 py-1 rounded-full"  
+          /> 
+          
+          <button
+            className="px-3 py-1 border-2 border-solid rounded-full"
+          >SEARCH</button>
+        </div>
+      </div>
+    </div>
   </div>)
 }
