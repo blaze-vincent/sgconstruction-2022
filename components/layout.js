e950f286a41b045e06ff7c6e569b3cd327500e87
Blaze Vincent <Blaze.Vincent@DRAKEHS.LOCAL>
2022-01-31 15:16:52 -0600

    Add favicons

--- components/layout.js
+++ components/layout.js
@@ -7,9 +7,14 @@ export default function Layout({children}){
       className="bg-neutral-300 min-h-screen flex flex-col overflow-visible"
     >
       <Head>
-        <title>nextjs w/ tailwindcss</title>
-        <meta name="description" content="bv nextjs template with tailwindcss" />
-        <link rel="icon" href="/favicon.ico" />
+        <title>SG Construction</title>
+        <meta name="description" content="SG Construction | Southeast Iowa General Contractors" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
+        <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
 
       <Header />
